edc0d6c34d711f21e0e425f5015d6a6c91188f00
unknown <alex.chinchi02@gmail.com>
2024-10-04 16:50:01 +0200

    calculadora

--- js/app.js
+++ js/app.js
@@ -3,4 +3,22 @@ const $ = selector => document.querySelector(selector);
 const $$ = selector => document.querySelectorAll(selector);
 
 // ELEMENTS
-const $result = $('#result');
+const $historial = $('#historial');
+const $resultado = $('#resultado');
+const $opera = $('#opera');
+
+$opera.addEventListener('click', comprobar);
+
+function comprobar(){
+
+    console.log($resultado.value);
+    if($resultado.value == 69){
+        $resultado.classList.add("blur");
+    }
+
+    let alert = document.createElement('div');
+    alert.innerHTML = ``
+}
+function resultadoCensura(){
+    console.log($resultado.value);
+}
